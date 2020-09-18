d4c13ce303099275d11f1236ad56f44e85495521
Tiffany Brand <brand2learn@gmail.com>
2020-09-18 10:43:29 -0400

    Update connection for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+let connection;
 
-let connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
 connection.connect(function (err) {
