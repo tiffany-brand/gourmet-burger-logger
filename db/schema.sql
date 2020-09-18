59adaccf05a95c4b689dba2691f20914029e278a
Tiffany Brand <brand2learn@gmail.com>
2020-09-18 14:17:05 -0400

    Update db schema for JawsDB

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,11 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
+
 CREATE TABLE burgers (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT AUTO_INCREMENT NOT NULL ,
   burger_name VARCHAR(255) NOT NULL,
   devoured TINYINT(1) NOT NULL,
+  createdAt TIMESTAMP NOT NULL,
   PRIMARY KEY (id)
 );
