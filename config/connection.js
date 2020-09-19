f48b44eb4c3661800e4b8e092773948c64260486
Tiffany Brand <brand2learn@gmail.com>
2020-09-18 20:17:12 -0400

    Add comments in config files

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,27 @@
 // Set up MySQL connection.
-const mysql = require("mysql");
+const mysql = require('mysql');
 let connection;
 
+// Set up db connection in local or remote db
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        port: 3306,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: "burgers_db"
-    });
+	connection = mysql.createConnection({
+		host: process.env.DB_HOST,
+		port: 3306,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: 'burgers_db'
+	});
 }
 
 // Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+connection.connect(function(err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
 });
 
 // Export connection for our ORM to use.
