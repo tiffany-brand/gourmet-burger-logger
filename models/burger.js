ce5e6d3050bf221371c2090ad870bdea8a12446a
Tiffany Brand <brand2learn@gmail.com>
2020-09-20 14:49:43 -0400

    Add comment

--- models/burger.js
+++ models/burger.js
@@ -2,19 +2,19 @@ const orm = require('../config/orm');
 
 // burger model with all of its query functions
 const burger = {
-	selectAll: function() {
+	selectAll: function () {
 		return orm.selectAll('burgers');
 	},
 	// The variables cols and vals are arrays.
-	insertOne: function(cols, vals) {
+	insertOne: function (cols, vals) {
 		return orm.insertOne('burgers', cols, vals);
 	},
-	updateOne: function(objColVals, condition) {
+	updateOne: function (objColVals, condition) {
 		return orm.updateOne('burgers', objColVals, condition);
 	},
-	deleteOne: function(condition) {
+	deleteOne: function (condition) {
 		return orm.deleteOne('burgers', condition);
 	}
 };
-
+// export the burger model to use in the burgers_controller
 module.exports = burger;
